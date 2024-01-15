8d1032d65d9d68a8db1b83c7660bd61823d11627
shufschmid <shufschmid@gmail.com>
2024-01-15 21:08:09 +0100

    added vuetify select with initial option

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,32 @@
 <script setup lang="js">
-
 const store = useButtenmostStore();
-
-
+let selected = ref(store.possibleShippingDays[0])
 </script>
 
 <template>
-  <v-btn>test Page</v-btn> 
+  <ul>
+    <li v-for="item in store.possibleShippingDays">
+      {{ item.text }}
+    </li>
+  </ul>
+
+  sel: {{ selected }}
+  <v-select
+    label="Select"
+    :items="store.possibleShippingDays"
+    v-model="selected"
+    return-object
+
+  ></v-select>
+
+  <v-btn>test Page</v-btn>
   <Signup v-show="store.isSaison"></Signup>
-  Saisonstartdate: 
-  
+
+  possible shipping days:
+  {{ store.possibleShippingDays }}
+  Saisonstartdate:
+
   {{ store.saisonStartString }}, isSaison: {{ store.isSaison }}
 
   <Verkaufsstellen></Verkaufsstellen>
-
-  
 </template>
--- store/ButtenmostStore.js
+++ store/ButtenmostStore.js
@@ -3,8 +3,9 @@ import { defineStore } from "pinia";
 export const useButtenmostStore = defineStore("buttenmost", {
   state: () => {
     return {
-      saisonStart: new Date("2023-09-25"),
-      saisonEnd: new Date("2024-11-12"),
+      saisonStart: new Date("2024-01-01"),
+      saisonEnd: new Date("2024-02-12"),
+      shippingWeekDay: "tue",
       heute: new Date(),
     };
   },
@@ -17,6 +18,23 @@ export const useButtenmostStore = defineStore("buttenmost", {
     saisonStartString() {
       const options = { year: "numeric", month: "long", day: "numeric" };
       return this.saisonStart.toLocaleDateString("de-DE", options);
+    },
+    possibleShippingDays() {
+      var result = [];
+      var days = { sun: 0, mon: 1, tue: 2, wed: 3, thu: 4, fri: 5, sat: 6 };
+      var day = days[this.shippingWeekDay];
+      var current = new Date(this.saisonStart);
+      current.setDate(current.getDate() + ((day - current.getDay() + 7) % 7));
+      while (current < this.saisonEnd) {
+        if (current > this.heute) {
+          result.push({
+            title: new Date(+current).toLocaleDateString("de-DE"),
+            value: new Date(+current),
+          });
+        }
+        current.setDate(current.getDate() + 7);
+      }
+      return result;
     },
   },
 });
